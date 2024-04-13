37811cc0e4d7219434824265b7c5383c9a92dc52
Soft 'n' Web <42813822+SoftNWeb@users.noreply.github.com>
2024-04-13 16:16:56 +0530

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,15 @@ import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
+import ReactGA from 'react-ga4';
 
 import Router from "./router";
 import i18n from "./translation";
+
+//const trackingIds = ['G-WK8FXW2821', 'G-SWV6BZ1KER', 'G-R6Q7DW4DDB', 'G-NX0QK8MD21', 'G-9FVCJH1EJ9'];
+
+ReactGA.initialize("G-41RE98HS2L");
+
 
 const App = () => (
   <BrowserRouter>
